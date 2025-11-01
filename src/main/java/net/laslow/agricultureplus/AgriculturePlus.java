ec9a38147cc6f4209a0d7e35fda2499278238bf5
Armi1014 <ardaiarmand@gmail.com>
2025-11-01 13:27:19 +0100

    Added XP spawn for crops.

--- src/main/java/net/laslow/agricultureplus/AgriculturePlus.java
+++ src/main/java/net/laslow/agricultureplus/AgriculturePlus.java
@@ -2,6 +2,9 @@ package net.laslow.agricultureplus;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
+import net.minecraft.block.Block;
+import net.minecraft.block.CropBlock;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,6 +15,19 @@ public class AgriculturePlus implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+        PlayerBlockBreakEvents.AFTER.register((world, playerEntity, blockPos, blockState, blockEntity) -> {
+            if (world.isClient()) return;
+            if (playerEntity.isCreative() || playerEntity.isSpectator()) return;
+            Block block = blockState.getBlock();
+            if (!(block instanceof net.minecraft.block.CropBlock)) return;
+            int age = blockState.get(CropBlock.AGE);
+            if (age < CropBlock.MAX_AGE) return;
+            int xp = 1;
+            net.minecraft.entity.ExperienceOrbEntity.spawn(
+                    (net.minecraft.server.world.ServerWorld) world,
+                    net.minecraft.util.math.Vec3d.ofCenter(blockPos),
+                    xp
+            );
+        });
 	}
 }
